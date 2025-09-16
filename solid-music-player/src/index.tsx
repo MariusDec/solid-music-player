119006f1a4b1483f21bc0ba7fc87e8cdbf2da2e4
Marius Donci <marius.donci@gmail.com>
2025-09-16 15:07:12 +0300

    Added initial project

---
+++ solid-music-player/src/index.tsx
@@ -0,0 +1,11 @@
+/* @refresh reload */
+import { render } from 'solid-js/web';
+import './index.css';
+import App from './App.tsx';
+
+const root = document.getElementById('root');
+if (root === null) {
+  throw new Error('Root element not found');
+}
+
+render(() => <App />, root);
